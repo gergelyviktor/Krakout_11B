656534312e3efc12a2f4360aac2c65fd5a0d5100
gergelyviktor <gergelyv@kkszki.hu>
2025-03-19 08:23:23 +0100

    Labda pattog

--- Krakout_11B/MainWindow.xaml.cs
+++ Krakout_11B/MainWindow.xaml.cs
@@ -12,17 +12,35 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
-namespace Krakout_11B
-{
+namespace Krakout_11B {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        double xSeb = 5;
+        double ySeb = 5;
+        public MainWindow() {
             InitializeComponent();
+            var ido = new DispatcherTimer();
+            ido.Interval = TimeSpan.FromMilliseconds(1);
+            ido.Tick += mozgatas;
+            ido.Start();
+        }
+
+        private void mozgatas(object sender, EventArgs e) {
+
+            var labdaX = Canvas.GetLeft(labda);
+            var labdaY = Canvas.GetTop(labda);
+            // nézzük a képernyő határait
+            if (labdaX > 950) xSeb = -5;
+            if (labdaX < 0) xSeb = 5;
+            if (labdaY > 550) ySeb = -5;
+            if (labdaY < 0) ySeb = 5;
+            // a labda mozgatása
+            Canvas.SetLeft(labda, labdaX + xSeb);
+            Canvas.SetTop(labda, labdaY + ySeb);
         }
     }
 }
