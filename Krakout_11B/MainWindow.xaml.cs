aff3440480d0265e9556251bbad8804a23d33ca0
gergelyviktor <gergelyv@kkszki.hu>
2025-03-26 09:38:46 +0100

    Alul kimegy

--- Krakout_11B/MainWindow.xaml.cs
+++ Krakout_11B/MainWindow.xaml.cs
@@ -22,21 +22,24 @@ namespace Krakout_11B {
     public partial class MainWindow : Window {
         double xSeb = 5;
         double ySeb = 5;
-        double alapSeb = 5;
+        double alapSeb = -5;
         int pontszam = 0;
         public MainWindow() {
             InitializeComponent();
 
             Cursor = Cursors.None;
-
-
-            var tegla = new Image();
-            tegla.Source = new BitmapImage(new Uri("tegla.jpg", UriKind.Relative));
-            tegla.Width = 90;
-            tegla.Height = 20;
-            tegla.Stretch = Stretch.Fill;
-            jatekter.Children.Add(tegla);
-
+            for (int j = 0; j < 5; j++) {
+                for (int i = 0; i < 10; i++) {
+                    var tegla = new Image();
+                    tegla.Source = new BitmapImage(new Uri("tegla.jpg", UriKind.Relative));
+                    tegla.Width = 90;
+                    tegla.Height = 20;
+                    tegla.Stretch = Stretch.Fill;
+                    Canvas.SetLeft(tegla, i * 100);
+                    Canvas.SetTop(tegla, j * 30);
+                    jatekter.Children.Add(tegla);
+                }
+            }
 
             labda.CacheMode = new BitmapCache();
             //// Sync rendering with frame refresh rate
@@ -53,16 +56,19 @@ namespace Krakout_11B {
         private void mozgatas(object sender, EventArgs e) {
             // a játékos mozgatása
             Canvas.SetLeft(jatekos, Mouse.GetPosition(jatekter).X);
+            // labda mozgatása
             var labdaX = Canvas.GetLeft(labda);
             var labdaY = Canvas.GetTop(labda);
             // nézzük a képernyő határait
             if (labdaX < 0 || labdaX > 950) xSeb *= -1;
             if (labdaY > 550) {
                 // ha a labda eléri a képernyő alját, vonjon le egy pontot
-                pontszam = 0;
-                lbPontszam.Content = 0;
-                Canvas.SetTop(labda, 0);
-                labdaY = 0;
+                lbPontszam.Content = --pontszam;
+                // rátesszük a labdát a játékosra
+                labdaX = Canvas.GetLeft(jatekos) + jatekos.Width / 2;
+                labdaY = Canvas.GetTop(jatekos) - labda.Height;
+                Canvas.SetLeft(labda, labdaX);
+                Canvas.SetTop(labda, labdaY);
                 ySeb = alapSeb;
             }
             if (labdaY < 0) ySeb *= -1;
@@ -76,8 +82,27 @@ namespace Krakout_11B {
                 && labdaY < jatekosY + jatekos.Height
                 ) {
                 ySeb *= -1.3;
-                lbPontszam.Content = ++pontszam;
+
             }
+
+            // ütközésvizsgálat a labda és a tégla között
+            foreach (var tegla in jatekter.Children.OfType<Image>()) {
+
+                var teglaX = Canvas.GetLeft(tegla);
+                var teglaY = Canvas.GetTop(tegla);
+                if (labdaX + labda.Width > teglaX
+                    && labdaX < teglaX + tegla.Width
+                    && labdaY + labda.Height > teglaY
+                    && labdaY < teglaY + tegla.Height
+                    ) {
+                    ySeb *= -1;
+                    jatekter.Children.Remove(tegla);
+                    lbPontszam.Content = ++pontszam;
+                    break;
+                }
+            }
+
+
             // a labda mozgatása
             Canvas.SetLeft(labda, labdaX + xSeb);
             Canvas.SetTop(labda, labdaY + ySeb);
